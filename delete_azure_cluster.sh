d2a70bea50636acb32fdf4b2f8afc48c00a4e949
Cihan B <cihangirb@hotmail.com>
2016-04-04 00:29:15 -0700

    comments

--- delete_azure_cluster.sh
+++ delete_azure_cluster.sh
@@ -2,7 +2,10 @@
 account="cb@cihangirbhotmail.onmicrosoft.com"
 
 azure login -u $account
+
+#set mode to asm
 azure config mode asm
+
 for ((i=1; i<=8; i++))
 do
 	echo "Working on deleting instance: $i"
