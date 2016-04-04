aa88afc9a8365dc7959132a536c8604d5319925d
Cihan B <cihangirb@hotmail.com>
2016-04-04 00:13:20 -0700

    added comment

--- delete_azure_cluster.sh
+++ delete_azure_cluster.sh
@@ -4,6 +4,6 @@ azure login -u cb@cihangirbhotmail.onmicrosoft.com
 azure config mode asm
 for ((i=1; i<=8; i++))
 do
-	echo "Working on instance: $i"
+	echo "Working on deleting instance: $i"
 	azure vm delete cbase-$i -q
 done
