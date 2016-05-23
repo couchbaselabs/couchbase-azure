61c9ae75246152bd9561e1c50dff5f0030b2bdac
Cihan B <cihangirb@hotmail.com>
2016-05-23 08:27:48 -0700

    added licensing text.

--- OSx_Scripts/delete_azure_cluster.sh
+++ OSx_Scripts/delete_azure_cluster.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Couchbase
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Author: Cihan Biyikoglu - github:(cihanb)
+
 #read settings
-source ./my_settings.sh
+source ./settings.sh
 
 #warning
 echo "WARNING: This will wipe out your cluster nodes, jumpbox and delete all your data on VMs starting with the `"$vm_name_prefix"` prefix. vnet "$vnet_name" will also be cleaned up if no other node on the same vnet remains. [y/n]"
